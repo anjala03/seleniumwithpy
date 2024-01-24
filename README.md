b1780df7c354987e5e169e67018ffe2e7d25201a
anjala03 <bhattaanjala13@gmail.com>
2024-01-24 16:49:10 +0545

    subheading and heading scraped from geek for geek

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ### Selenium with python
+This project describes how to scrape an web browser using selenium with  chromedriver. 
--- sel.py
+++ sel.py
@@ -1,19 +1,37 @@
-# from selenium import webdriver
-#from webdriver_manager.chrome import ChromeDriverManager
-#driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver.get("https://www.geeksforgeeks.org/")
-#driver.quit()
-
-
-#from selenium.webdriver.chrome.service import Service
-#from selenium import webdriver
-#
-#service = Service(executable_path="./chromedriver.exe")
-#driver = webdriver.Chrome(service=service)
-#driver.get("https://www.geeksforgeeks.org/")
-#
-
 from selenium import webdriver
-driver= webdriver.Chrome()
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+import time
+#elem = driver.find_elements(By.CLASS_NAME , "video-card-heading")
+#for i in elem:
+#    print(i.text)
+#time.sleep(5)
+#driver.get("https://www.google.com/")
+#print(driver.title)
+#time.sleep(5)
+#driver.back()
+#print(driver.title)
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()) , options=options)
+
+
 driver.get("https://www.geeksforgeeks.org/")
+print(driver.title)
+
+elements= driver.find_elements(By.CLASS_NAME, "ant-col-offset-1" )
+for element in elements: 
+    heading= element.find_element(By.CLASS_NAME, "ant-col-23" )
+    # heading= heading.find_element(By.CLASS_NAME, "video-card-heading")
+    cards= element.find_elements(By.CLASS_NAME, "gfg_home_page_topic_card_cover" )
+    # print(f"******** Heading= {heading.text} **********")
+    for card in cards:
+         subhead = card.find_element(By.CLASS_NAME, "ant-row" )
+         print(f"The subheadings are: {subhead.text}")
+
 driver.quit()
+
+
+
+
